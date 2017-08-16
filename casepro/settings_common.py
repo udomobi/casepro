8c3ef98a679f593d9f8160f812e75f68c667f5cb
John Cordeiro <john.dalton@ilhasoft.com.br>
2017-08-16 15:29:54 -0300

    Update handle message timedelta to 5 seconds

--- casepro/settings_common.py
+++ casepro/settings_common.py
@@ -475,7 +475,7 @@ CELERYBEAT_SCHEDULE = {
     },
     'message-handle': {
         'task': 'dash.orgs.tasks.trigger_org_task',
-        'schedule': timedelta(seconds=20),
+        'schedule': timedelta(seconds=5),
         'args': ('casepro.msgs.tasks.handle_messages', 'sync')
     },
     'squash-counts': {
