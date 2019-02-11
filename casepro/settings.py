4f35a0f983a0521b8fa091d343b162b58c5274d0
Eduardo Douglas <edudouglas@gmail.com>
2019-02-11 19:53:45 -0300

    Fix AWS config

--- casepro/settings.py
+++ casepro/settings.py
@@ -67,13 +67,17 @@ CACHES = {
 CELERY_RESULT_BACKEND = BROKER_URL
 
 RAVEN_CONFIG = {
-    'dsn': config('RAVEN_CONFIG'),
+    'dsn': config('RAVEN_CONFIG', default=''),
 }
 
-AWS_ACCESS_KEY = config('AWS_ACCESS_KEY')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_S3_BUCKET = config('AWS_S3_BUCKET')
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default='')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default='')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default='')
+
+AWS_QUERYSTRING_AUTH = False
+AWS_DEFAULT_ACL = ''
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 COMPRESS_ENABLED = config('COMPRESS_ENABLED', default=True, cast=bool)
 COMPRESS_OFFLINE = config('COMPRESS_OFFLINE', default=True, cast=bool)
