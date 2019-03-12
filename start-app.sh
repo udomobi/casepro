731bcbd11a14c1a9b5d42aca424221f5d6b55f86
Eduardo Douglas <edudouglas@gmail.com>
2019-03-12 15:18:56 -0300

    Merge production with fix-tests

--- start-app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd $WORKDIR
-python3.6 manage.py migrate --noinput
-python3.6 manage.py collectstatic --noinput
-python3.6 manage.py compress --extension=.haml,.html
-python3.6 manage.py compilemessages
-
-gunicorn casepro.wsgi --log-config gunicorn-logging.conf -c gunicorn.conf.py --workers 4
